a5b435a3c7259619d822cb2ffca9304a88580fea
davi <davidmarques856@gmail.com>
2018-12-10 17:02:30 +0000

    Day 1 second puzzle done

--- 1.1/2.c
+++ 1.1/2.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h>              /* For atoi */
 
-#define INPUT "input"
 #define BUFFSIZE 10
+
 /* cat INPUT | wc -l */
-#define FREQSTORESIZE 1028
+#define FREQSTORESIZE 10280000
 
 /* Check if frequency is in list */
 int checkfreqlist(int *, int, int); 
 
-void printbuffer(int*, int);
+int main(int argc, char **argv){
 
-int main(){
   /* i and j are two counters */
   /* ch stores the character of a file */
   int ch, i=0, j=0, sum=0, dfreq, ENDFLAG=0;
@@ -25,7 +24,7 @@ int main(){
   freqstore = (int *) malloc(FREQSTORESIZE);
 
   FILE *fp;
-  fp = fopen(INPUT, "r");
+  fp = fopen(argv[1], "r");
 
   /* Reads File continuously */
   while (ENDFLAG==0){
@@ -39,6 +38,7 @@ int main(){
         else{
         /* Adds null terminator */
         *buff = '\0';
+
         /* Decrements pointer to original position */
         buff -= i;
         /* Sets i back to 0 */
@@ -47,45 +47,25 @@ int main(){
         /* Stores Frequency */
         dfreq = atoi(buff); 
 
-        /* Sums value to sum one by one */
-        while (dfreq != 0){
+        sum += dfreq;
 
-          /* printf("midsum: %d\n", sum); */
-          /* Checks if it is negative or positive */
-          if(dfreq < 0){
-            sum -= 1;
-            dfreq +=1;
-          }else{
-            sum += 1;
-            dfreq -=1;
-          }
+        ENDFLAG = checkfreqlist(freqstore, sum, j);
 
-          /* printf("  midsum: %d\n", sum); */
-          /* Checks if freq is repeated */
-          /* j is the size of the frquency store buffer */
-          ENDFLAG = checkfreqlist(freqstore, sum, j);
-
-          if (ENDFLAG){
-            break;
-          }
+        if (ENDFLAG){
+        break;
         }
 
-        /* printf("sum: %d\n", sum); */
         *(freqstore+j) = sum;
-        /* printbuffer(freqstore, j); */
         j++;                     
       }
     }
 
-      /* Reads File again  */
-      /* There is probably a better way to do this */
-      fp -= FREQSTORESIZE;
+    /* Re-read file */
+    rewind(fp);
   }
 
   /* Prints answer */
   printf("ANSWER: %d\n", sum);
-  /* free(buff); */
-  /* free(freqstore); */
   return 1;
 }
 
@@ -96,24 +76,9 @@ int checkfreqlist(int* freqstore, int sum, int j){
     /* Checks */
     while(i < j){
       if (*(freqstore+i) == sum){
-        printf("found ans: %d == %d\n", sum, *(freqstore+i));
         return 1;
       }
       i++;
     }
     return 0;
   }
-
-void printbuffer(int* freqstore, int j){
-    /* Returns boolean */
-    int i = 0;
-
-    printf("BUFFER: ");
-    /* Checks */
-    while(i <= j){
-      printf("%d ", *(freqstore+i));
-      i++;
-    }
-    printf("\n");
-  }
-
