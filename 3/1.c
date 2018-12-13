5a08a014997ef1031d2b487d7ddf8dbd9fe3ba00
davi <davidmarques856@gmail.com>
2018-12-13 19:02:20 +0000

    Finished third day first challenfe

--- 3/1.c
+++ 3/1.c
@@ -1,26 +1,63 @@
+/* Awk and sed were used to filter the intput file  */
+/* dl  dt  w  h */
+/* 333 27  15 24 */
+/* 107 608 16 18 */
+/* 695 587 18 16 */
+/* 435 778 25 13 */
+/* 562 221 12 16 */
+
 #include<stdio.h>
 
 /* No reason why its 30 */
 #define BUFFERSIZE 30
-#define MATSIZE 1000
-
-/* Gets height and other stuff from the string */
-void evalstring(char *buff);
-
-/* Increments values in the matrix */
-void populmatrix(int dl, int dt, int w,  int h);
+#define MATSIZE 1001
 
 int main(int argc, char **argv){
 
   FILE *fp;
   fp = fopen(argv[1], "r");
 
-  int mat[MATSIZE][MATSIZE];
+  int mat[MATSIZE][MATSIZE] = {0};
+
+  int dl=0, dt=0, w=0, h=0;
+
+  int ystart=0, yend=0, xstart=0, xend=0;
+
+  int common=0;
 
   /* Stores lines */
-  char buff[BUFFERSIZE];
+  char buff[BUFFERSIZE]="";
 
   while(fgets(buff, BUFFERSIZE, fp) != NULL){
-    printf("%s", buff);
+    sscanf(buff, "%d %d %d %d\n", &dl, &dt, &w, &h);
+    /* printf("%d %d %d %d\n", dl, dt, w, h); */
+
+    /* Start and end Y coordinate */
+    ystart = MATSIZE - dt;
+    yend = ystart - h;
+
+    /* Start and end X coordinate */
+    xstart = dl;
+    xend = xstart + w;
+
+    /* Populates the matrix */
+    while (ystart > yend){
+      while(xstart < xend){
+        mat[xstart][ystart]++;
+        xstart++;
+      }
+    ystart--;
+    xstart -= w;
+    }
   }
+
+  /* Count the common square inches */
+  for(int i = 0; i < MATSIZE; i++){
+    for(int j = MATSIZE; j > 0; j--){
+      if (mat[i][j] >= 2){
+        common++;
+      }
+    }
+  }
+  printf("%d\n", common);
 }
