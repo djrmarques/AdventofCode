a4d5a9216974baf02824da38200bcd44bd352822
davi <davidmarques856@gmail.com>
2018-12-15 13:36:16 +0000

    Done in Python

--- 5/2.py
+++ 5/2.py
@@ -1,12 +1,12 @@
 # Tis one gives the correct answer
 
-file = "5/test-input"
+file = "5/input"
 minsize=50000
 
-with open(file, "r") as f:
-    pol_str = f.readline()
-
-pol = [a for a in pol_str[:-1]]
+def get_file():
+    with open(file, "r") as f:
+        pol_str = f.readline()
+    return [a for a in pol_str[:-1]]
 
 def compcase(c1, c2):
     if (c1 == 0 or c2 == 0):
@@ -17,14 +17,17 @@ def compcase(c1, c2):
         return 0
 
 for pchar in "abcdefghijklmnopqrstuvwxyz":
-
     FLAG=1
+    # Reads the characters
+
+    pol = get_file()
+    # Remove the characters from the 
+    pol = [a for a in pol if (a.upper() != pchar.upper())]
+
     while(FLAG):
         FLAG=0
         i=0
         for i in range(len(pol[:-1])):
-            if compcase(pchar, pol[i]):
-                pol[i] = 0
             while(compcase(pol[i], pol[i+1])):
                 pol[i] = 0
                 pol[i+1] = 0
@@ -37,6 +40,7 @@ for pchar in "abcdefghijklmnopqrstuvwxyz":
         min_probchar = pchar
         min_pol = pol
         minsize=len(pol)
+    
 
 print(min_probchar)
 print(min_pol)
