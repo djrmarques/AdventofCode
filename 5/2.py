eef3c88928e581dc7d9a6d6cf1456ec23996effe
davi <davidmarques856@gmail.com>
2018-12-15 12:01:02 +0000

    Started second part

---
+++ 5/2.py
@@ -0,0 +1,44 @@
+# Tis one gives the correct answer
+
+file = "5/test-input"
+minsize=50000
+
+with open(file, "r") as f:
+    pol_str = f.readline()
+
+pol = [a for a in pol_str[:-1]]
+
+def compcase(c1, c2):
+    if (c1 == 0 or c2 == 0):
+        return 0 
+    elif ((c1.isupper() != c2.isupper()) and (c1.upper() == c2.upper())):
+        return 1
+    else:
+        return 0
+
+for pchar in "abcdefghijklmnopqrstuvwxyz":
+
+    FLAG=1
+    while(FLAG):
+        FLAG=0
+        i=0
+        for i in range(len(pol[:-1])):
+            if compcase(pchar, pol[i]):
+                pol[i] = 0
+            while(compcase(pol[i], pol[i+1])):
+                pol[i] = 0
+                pol[i+1] = 0
+                FLAG=1
+            i+=1
+
+        pol = [a for a in pol if a != 0]
+
+    if len(pol) < minsize:
+        min_probchar = pchar
+        min_pol = pol
+        minsize=len(pol)
+
+print(min_probchar)
+print(min_pol)
+minsize
+
