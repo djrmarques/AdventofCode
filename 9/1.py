215b5871ac48be074705ef8a0ee5f9a61cf4c6c3
David <davidmarques856@gmail.com>
2018-12-19 16:38:55 +0000

    Finished part 2

--- 9/1.py
+++ 9/1.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 
 class game:
 
@@ -9,39 +11,27 @@ class game:
         # Runs the game until last marble
 
         # Placeholder for the games marbles
-        L = [0]
+        L = deque([0])
         n_marble = 1
-        current_marble_pos = 0
 
         while 1:
             # Runs once for every player
             for player in range(1, n_players+1):
 
                 if not self.is_m23(n_marble):
-                    if (current_marble_pos + 2) <= len(L):
-                        current_marble_pos += 2
-                    else:
-                        current_marble_pos = current_marble_pos + 2 - len(L)
-                    L.insert(current_marble_pos, n_marble)
+                    L.rotate(-1)
+                    L.append(n_marble)
+
                 else:
                     self.player_scores[player] += n_marble
 
-                    # Remove the 7th counter clockwise marble
-                    if current_marble_pos - 7 < 0:
-                        remove_marble = current_marble_pos - 7 + len(L)
-                    else:
-                        remove_marble = current_marble_pos - 7
+                    L.rotate(7)
+                    self.player_scores[player] += L.pop()
+                    L.rotate(-1)
 
-                    self.player_scores[player] += L[remove_marble]
-                    del L[remove_marble]
-
-                    current_marble_pos = remove_marble 
-
-                # print("{}: {}".format(player, L))
                 if n_marble >= l_marble_val:
                     return self.player_scores
                 else:
-                    # Increments marble number
                     n_marble += 1
 
     def is_m23(self, n):
@@ -58,10 +48,12 @@ def test_routine():
             n_players, l_marble_val, hscore = line.strip().split()
             g = game(n_players, l_marble_val)
             results = g.run_game(int(n_players), int(l_marble_val))
-            # print("{:<6} {}".format(max(results.values()),  int(hscore)))
+            print("{:<6} {}".format(max(results.values()),  int(hscore)))
             print(max(results.values()) ==  int(hscore))
 
-def solve():
+# test_routine()
+
+def solve_part1():
     file = '9/input'
     with open(file, "r") as f:
         n_players, l_marble_val = f.readline().strip().split()
@@ -69,6 +61,15 @@ def solve():
         results = g.run_game(int(n_players), int(l_marble_val))
         print(max(results.values()))
 
-solve()
+# 409832
+# solve_part1()
 
+def solve_part2():
+    file = '9/input'
+    with open(file, "r") as f:
+        n_players, l_marble_val = f.readline().strip().split()
+        g = game(n_players, l_marble_val)
+        results = g.run_game(int(n_players), int(l_marble_val)*100)
+        print(max(results.values()))
 
+# solve_part2()
